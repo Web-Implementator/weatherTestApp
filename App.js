5774c97906a132b9b1e216e3b9b7c09e5b5c1370
Web Implementator <web-implementator@yandex.ru>
2020-11-24 14:05:07 +0700

    Background, template header, add fonts, add svg icons

--- App.js
+++ App.js
@@ -1,114 +1,152 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
 import {
+  SegmentedControlIOS,
+  LayoutAnimation,
   SafeAreaView,
+  StatusBar,
   StyleSheet,
   ScrollView,
+  Image,
   View,
+  TextInput,
   Text,
-  StatusBar,
+  Keyboard,
+  KeyboardAvoidingView,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import LinearGradient from 'react-native-linear-gradient';
+import { BlurView, VibrancyView } from "@react-native-community/blur";
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      blurOpacity: false,
+    };
+  }
+
+  render() {
+    return (
+      <LinearGradient colors={['#f6bfaf', '#eac895', '#e0d180']} style={styles.container}>
+
+        <View style={[styles.mb10, styles.plr10]}>
+          <View style={[styles.logo, styles.mb5]}>
+            <Text style={[styles.titleMini, {}]}>W</Text>
+            <Text style={[styles.titleMini, {position: 'relative', left: -5}]}>W</Text>
           </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+
+          <Text style={[styles.titleFull, styles.mb15]}>whatweather?</Text>
+
+          <Text style={[styles.textInfo, styles.mb10]}>Type in your location and we tell you what weather to expect.</Text>
+          <TextInput
+            style={styles.inputCity}
+            placeholder="Your city"
+            placeholderTextColor="#000"
+            onFocus={this.focusInput}
+            onEndEditing={this.focusInput}
+          />
+        </View>
+
+        <View style={{position: 'relative', flex: 1}}>
+          <BlurView
+            style={[styles.absolute, { opacity: this.state.blurOpacity==true?1:0 }]}
+            blurType="light"
+            blurAmount={10}
+            reducedTransparencyFallbackColor="white"
+          />
+
+          <View style={[styles.mt20, styles.mb20, styles.textCenter]}>
+            <Text style={{fontFamily: "Roboto-Black",fontSize: 50}}>25 C</Text>
+          </View>
+        </View>
+      </LinearGradient>
+    );
+  }
+
+  focusInput = () => {
+    // LayoutAnimation.easeInEaseOut();
+    this.setState({ blurOpacity: !this.state.blurOpacity })
+  }
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1,
+    paddingTop: 50
   },
-  engine: {
-    position: 'absolute',
+  logo: {
+    flexDirection: 'row', flexWrap: 'wrap'
+  },
+  titleMini: {
+    fontFamily: "Roboto-Black",
+    fontSize: 30
+  },
+  titleFull: {
+    fontFamily: "Roboto-Bold",
+    fontSize: 14
+  },
+  textInfo: {
+    fontFamily: "Roboto-Regular"
+  },
+  inputCity: {
+    position: 'relative',
+    zIndex: 0,
+
+    fontFamily: "Roboto-Medium",
+    fontSize: 20,
+
+    height: 60,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    paddingLeft: 15,
+    paddingRight: 15,
+
+    //ios
+    shadowOpacity: 0.4,
+    shadowRadius: 4,
+    shadowOffset: {
+      height: 0,
+      width: 0
+    },
+    //android
+    elevation: 3
+  },
+
+  textCenter: {
+    alignItems: 'center'
+  },
+  absolute: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    bottom: 0,
     right: 0,
+    zIndex: 2,
   },
-  body: {
-    backgroundColor: Colors.white,
+
+  plr10: {
+    paddingLeft: 10,
+    paddingRight: 10
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  mt5: {
+    marginTop: 5
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
+  mt20: {
+    marginTop: 20
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
+  mb5: {
+    marginBottom: 5
   },
-  highlight: {
-    fontWeight: '700',
+  mb10: {
+    marginBottom: 10
   },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  mb15: {
+    marginBottom: 15
   },
+  mb20: {
+    marginBottom: 20
+  }
 });
 
 export default App;
